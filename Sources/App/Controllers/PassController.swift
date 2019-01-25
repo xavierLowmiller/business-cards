41b519a06964b1765e95b19340bfd7397c30327c
Xaver Lohmüller <xaver.lohmueller@me.com>
2019-01-25 19:52:51 +0100

    Fix semantics of passType

--- Sources/App/Controllers/PassController.swift
+++ Sources/App/Controllers/PassController.swift
@@ -1,5 +1,9 @@
 import Vapor
 
+private extension String {
+    static let passType = "pass.de.adorsys.businesscard"
+}
+
 final class PassController {
     struct PushQueryResponse: Content {
         let lastUpdated: String
@@ -9,6 +13,7 @@ final class PassController {
     func getPasses(_ req: Request) throws -> Future<PushQueryResponse> {
         let deviceID: String = try req.parameters.next()
         let passType: String = try req.parameters.next()
+        guard passType == .passType else { throw Abort(.badRequest) }
 
         if let tag: String = try? req.query.get(at: "passesUpdatedSince") {
             print(tag)
@@ -23,39 +28,19 @@ final class PassController {
             }
     }
 
-//    private func getPasses(_ request: Request) throws -> ResponseRepresentable {
-//        guard let deviceID = request.parameters[deviceIdKey]?.string,
-//            let passTypeIdentifier = request.parameters[passTypeKey],
-//            passTypeIdentifier.string == passID
-//            else { throw Abort.badRequest }
-//
-//        if let tag = request.query?["passesUpdatedSince"]?.string {
-//            print(tag)
-//        }
-//
-//        let serialNumbers = try PushAssociation.makeQuery()
-//            .filter("device_id", deviceID)
-//            .all()
-//            .map({ $0.passID })
-//        if serialNumbers.isEmpty {
-//            return Response(status: .noContent)
-//        } else {
-//            var json = JSON()
-//            try json.set("lastUpdated", "")
-//            try json.set("serialNumbers", serialNumbers.makeNode(in: nil))
-//            return json
-//        }
-//    }
-
     /// Saves a decoded `PushAssociation` to the database.
     func create(_ req: Request, json: [String: String]) throws -> Future<Response> {
         let deviceID: String = try req.parameters.next()
         let passType: String = try req.parameters.next()
         let passID: String = try req.parameters.next()
-        guard let pushID = json["pushToken"] else { throw Abort(.badRequest) }
+        
+        guard let pushID = json["pushToken"],
+            passType == .passType
+            else { throw Abort(.badRequest) }
 
         return PushAssociation.query(on: req)
             .filter(\.deviceID, .equal, deviceID)
+            .filter(\.passType, .equal, .passType)
             .filter(\.passID, .equal, passID)
             .count()
             .map { $0 != 0 }
